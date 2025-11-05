cc7415fd945c95cba4702c9e700280185744dff3
SAMIR EL ALAMI <elalamisamirr@gmail.com>
2025-11-05 15:25:31 +0100

    separate html pages & add more pictures

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
 html {
     scroll-behavior: smooth;
 }
-
-function myMap() {
-    var mapProp= {
-        center:new google.maps.LatLng(51.508742,-0.120850),
-        zoom:5,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-
-}
